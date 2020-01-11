910988cf67cb5302242bc17d701943e3034e96c0
daliPilot <45625971+daliPilot@users.noreply.github.com>
2020-01-11 17:39:46 +0100

    Update HelloWorld.java

--- src/main/java/com/xavki/HelloWorld.java
+++ src/main/java/com/xavki/HelloWorld.java
@@ -2,7 +2,7 @@ package com.xavki;
 public class HelloWorld {
   public static void main(String[] args) {
     HelloWorld myWorld = new HelloWorld();
-    String said = myWorld.sayHelloTo("World, dali");
+    String said = myWorld.sayHelloTo("World, Mr dali");
     System.out.println(said);
     }
   public String sayHelloTo(String to) {
